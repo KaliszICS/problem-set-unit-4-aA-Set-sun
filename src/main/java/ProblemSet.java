fc2d43fe3d524a7a11ede227c55ff30a9353c90a
A.Set <343303426@tdsb.ca>
2026-04-27 13:58:43 -0400

    well, he's never gonna be president now

--- src/main/java/ProblemSet.java
+++ src/main/java/ProblemSet.java
@@ -18,6 +18,9 @@ public class ProblemSet {
 
 		System.out.print("Input a number of rounds to play: ");
 		System.out.println(selectMenu());
+
+		input.close();
+		
 	}
 
 	public static int inputChecker() { //mostly functional, just weird spacing
@@ -51,56 +54,85 @@ public class ProblemSet {
 		int points = 0;
 		int correctRound = 0;
 
-		System.out.println("What Range would you like to play between (#-#)?");
+		//range selection
+		System.out.println("What Range would you like to play between (#-#)?"); //range
 		
-		String range = input.nextLine();
+		String range = findRange();
+		int greatNum = findGreatRange(range);
+		int lessNum = findLowRange(range);
+		
+		while (greatNum - lessNum == 0) { //if your numbers are wack
+			range = "";
+			System.out.println("Invalid Input!");
 
-			//(-5 - (-5)) -> 3 -
-			//(-5 - 5) or (5 - (-5))-> 2 -
-			//(5 - 5) -> 1 -
+			System.out.println("What Range would you like to play between (#-#)?");
+		
+			range = findRange();
+			greatNum = findGreatRange(range);
+			lessNum = findLowRange(range);
+		} 
+		int median = greatNum/2;
+		double firstMedian = median;
+		double secondMedian = median;
+		String combinedMedian;
+		boolean doubleMedian = false; 
 
-			String greatRange ="";
-			String lessRange="";
+		if (!(greatNum%2 == 0)) {
+			firstMedian = Math.floor(median);
+			secondMedian = Math.ceil(median);
 
-		//if (range.startsWith("-")) {//(-5 - 5) 
-		// 	greatRange = range.substring(range.indexOf(range.indexOf("-"), range.indexOf("-") + 1), range.length());
-		// 	lessRange = range.substring(0, range.lastIndexOf(range.indexOf("-"), range.indexOf("-") + 1));
-		// }
-		// System.out.println(lessRange + " yey " + greatRange);
+			combinedMedian = firstMedian + ", " + secondMedian;
+			doubleMedian = true;
+
+		} else {
+			combinedMedian = "" + median;
+		}
+		//end range finding
+
+		
 
         //round loop
 		for (int currentRound = 1; currentRound < roundNum + 1; currentRound++) {
 			System.out.println("Round " + currentRound + ":");
 
-			System.out.println("Please select High, Low, or Even:" + "\n" + "1. High (11 to 20)" + "\n" + "2. Low (0 to 9)" + "\n" + "3. Even (10)" + "\n");
+			System.out.println("Please select High, Low, or Even:" + "\n" + "1. High (" + combinedMedian + " to " + greatNum + ")" + "\n" + "2. Low (" + lessNum + " to " + combinedMedian + ")" + "\n" + "3. Even (" + combinedMedian + ")" + "\n");
 
 			//checking for input
 			while (!(input.hasNextInt())) {
 				input.nextLine(); //clear scanner
 				System.out.println("Invalid Input!");
 
-				System.out.println("Please select High, Low, or Even:" + "\n" + "1. High (11 to 20)" + "\n" + "2. Low (0 to 9)" + "\n" + "3. Even (10)" + "\n");
+				System.out.println("Please select High, Low, or Even:" + "\n" + "1. High (" + combinedMedian + " to " + greatNum + ")" + "\n" + "2. Low (" + lessNum + " to " + combinedMedian + ")" + "\n" + "3. Even (" + combinedMedian + ")" + "\n");
 			}
 			int choice = input.nextInt();
 
             //checking the input
-			while (choice <= 0 && choice < 3) {
+			while (choice <= 0 || choice > 3) {
 				input.nextLine(); //clear scanner
 				System.out.println("Invalid Input!");
 
-				System.out.print("Please select High, Low, or Even:" + "\n" + "1. High (11 to 20)" + "\n" + "2. Low (0 to 9)" + "\n" + "3. Even (10)" + "\n");
+				System.out.print("Please select High, Low, or Even:" + "\n" + "1. High (" + combinedMedian + " to " + greatNum + ")" + "\n" + "2. Low (" + lessNum + " to " + combinedMedian + ")" + "\n" + "3. Even (" + combinedMedian + ")" + "\n");
 				choice = input.nextInt();
 			}
 
             String result = "incorrect";
-			int num = randomNumber(0, 20);
+			int num = randomNumber(lessNum, greatNum);
 
 			if (choice == 1) {
 
-				if (num >= 11 && num <= 20) {
-				    result = "correct";
-					points++; 
-					correctRound++;
+				if (doubleMedian = true) { //check if there's two medians
+					if (num >= firstMedian || num >= secondMedian && num <= greatNum) {
+				 		result = "correct";
+						points++; 
+						correctRound++;
+					}
+				}
+				else if (doubleMedian = false) {
+					if (num >= median && num <= greatNum) { //only one median
+						result = "correct";
+						points++; 
+						correctRound++;
+					}
 				}
 				else {
 					result = "incorrect";
@@ -109,11 +141,20 @@ public class ProblemSet {
 
 			if (choice == 2) {
 
-				if (num >= 0 && num <= 9) {
-				    result = "correct";
-					points++; 
-					correctRound++;
+				if (doubleMedian = true) { //check if there's two medians
+					if (num <= firstMedian || num <= secondMedian && num >= lessNum) {
+				 		result = "correct";
+						points++; 
+						correctRound++;
+					}
+				} 
+				else if (doubleMedian = false) {
+					if (num <= median && num >= lessNum) {
+						result = "correct";
+						points++; 
+						correctRound++;
 
+					}
 				}
 				else {
 					result = "incorrect";
@@ -121,10 +162,20 @@ public class ProblemSet {
 			 } //low
 
 			if (choice == 3) {
-				if (num == 10) {
-				    result = "correct";
-					points++; 
-					correctRound++;
+
+				if (doubleMedian = true) { //check if there's two medians
+					if (num == firstMedian || num == secondMedian) {
+				 		result = "correct";
+						points++; 
+						correctRound++;
+					}
+				}
+				else if (doubleMedian = false) {
+					if (num == median) {
+						result = "correct";
+						points++; 
+						correctRound++;
+					}
 				}
 				else {
 					result = "incorrect";
@@ -135,12 +186,17 @@ public class ProblemSet {
 			System.out.println("Current Score: " + points);
 		}
 		System.out.println("Total Score: " + points);
-		System.out.println("Congratulations you got " + correctRound + " out of " + roundNum + " rounds right!");
+		if (correctRound >= roundNum/2) {
+		return ("Congratulations you got " + correctRound + " out of " + roundNum + " rounds right!");
+		}
+		else {
+			return "You got " + correctRound + " out of " + roundNum + " rounds correct. Better Luck next time.";
 
-		return "yay";
+		}
+		
 	}
 
-	public static int randomNumber(int num1, int num2) { //fix these numbers
+	public static int randomNumber(int num1, int num2) { 
 
 		Random random = new Random();
 
@@ -154,11 +210,77 @@ public class ProblemSet {
 
 		} while (num < less && num == 0);
 		return num;
-
-
-	}
-	
-	
 	}
 
+	public static String findRange() {
+		Scanner input = new Scanner(System.in);
+		String range;
+		//checking range
+		while (!(input.hasNextLine())) {
+			range = "";			
+			System.out.println("Invalid Input!");
+
+			System.out.println("What Range would you like to play between (#-#)?");
+		}
+		range = input.nextLine();
+
+		range = range.replace(" ", ""); //take out any spaces
+
+		while (!(range.contains("-"))) {
+			range = "";
+			System.out.println("Invalid Input!");
+
+			System.out.println("What Range would you like to play between (#-#)?");
+			range = input.nextLine();
+
+		}
+		return range;
+
+	}
+
+	public static int findGreatRange (String range) {
+		String greatRange = "";
+		if (range.startsWith("-")) {//has negative
+			int firstDash = range.indexOf('-');
+			int secondDash = range.lastIndexOf('-');
+
+			if (firstDash != secondDash) { //both negative
+				greatRange = range.substring(range.lastIndexOf("-"));
+
+			} 
+			else { //one negative
+				greatRange = range.substring(range.lastIndexOf("-") + 1);
+			}
+		}
+		else { //both positive
+			greatRange = range.substring(range.lastIndexOf("-") + 1);
+		}
+		int greatNum = Integer.parseInt(greatRange);
+		return greatNum;
+		
+	}
+
+	public static int findLowRange (String range) {
+		String lessRange= "";
+		if (range.startsWith("-")) {//has negative
+			int firstDash = range.indexOf('-');
+			int secondDash = range.lastIndexOf('-');
+
+			if (firstDash != secondDash) { //both negative
+				lessRange = range.substring(0, range.indexOf("-", range.indexOf("-") + 1));
+			} 
+			else { //one negative
+			lessRange = range.substring(0, range.lastIndexOf("-"));
+			}
+		}
+		else { //both positive
+			lessRange = range.substring(0, range.lastIndexOf("-"));
+		}
+
+		int lessNum = Integer.parseInt(lessRange);
+		return lessNum;
+	
+	}
+	
+} //what was THAT
 
